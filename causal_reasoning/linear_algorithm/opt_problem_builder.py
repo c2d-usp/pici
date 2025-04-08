3b646839bdb4553c64781f3889a7cbe473d669ef
Daniel Lawand <daniel.lawand@gmail.com>
2025-04-08 11:00:54 -0300

    Use node attributes instead of calling dictionary

--- causal_reasoning/linear_algorithm/opt_problem_builder.py
+++ causal_reasoning/linear_algorithm/opt_problem_builder.py
@@ -3,6 +3,7 @@ from scipy.optimize import linprog
 import pandas as pd
 
 from causal_reasoning.graph.graph import Graph
+from causal_reasoning.graph.node import Node
 from causal_reasoning.linear_algorithm.linear_constraints import generate_constraints
 from causal_reasoning.linear_algorithm.obj_function_generator import ObjFunctionGenerator
 
@@ -11,9 +12,9 @@ class OptProblemBuilder:
     def builder_linear_problem(
         graph: Graph,
         df: pd.DataFrame,
-        intervention: str,
+        intervention: Node,
         intervention_value: int,
-        target: str,
+        target: Node,
         target_value: int,
     ):
         objFG = ObjFunctionGenerator(
@@ -23,19 +24,13 @@ class OptProblemBuilder:
             intervention_value=intervention_value,
             target=target,
             target_value=target_value,
-            empiricalProbabilitiesVariables=[],
-            mechanismVariables=[],
-            conditionalProbabilities={},
-            debugOrder=[],
         )
         objFG.find_linear_good_set()
         mechanisms = objFG.get_mechanisms_pruned()
         objFunctionCoefficients = objFG.build_objective_function(mechanisms)
 
-        interventionLatentParent = objFG.graph.graphNodes[
-            objFG.intervention
-        ].latentParent
-        cComponentEndogenous = objFG.graph.graphNodes[interventionLatentParent].children
+        interventionLatentParent = objFG.intervention.latentParent
+        cComponentEndogenous = interventionLatentParent.children
         consideredEndogenousNodes = list(
             (set(cComponentEndogenous) & set(objFG.debugOrder)) | {objFG.intervention}
         )
