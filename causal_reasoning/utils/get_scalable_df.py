54025121c6f09494dd32a14b72af22bae265d1ab
Daniel Lawand <daniel.lawand@gmail.com>
2025-06-16 16:21:05 -0300

    update get dataframes

--- causal_reasoning/utils/get_scalable_df.py
+++ causal_reasoning/utils/get_scalable_df.py
@@ -12,6 +12,8 @@ def getScalableDataFrame(M: int, N: int):
         scalable_csv_path = Examples.CSV_N4M1.value
     elif (N == 5 and M == 1):
         scalable_csv_path = Examples.CSV_N5M1.value
+    elif (N == 6 and M == 1):
+        scalable_csv_path = Examples.CSV_N6M1.value
     elif (N == 1 and M == 2):
         scalable_csv_path = Examples.CSV_N1M2.value    
     elif (N == 2 and M == 2):
@@ -30,25 +32,17 @@ def getScalableDataFrame(M: int, N: int):
         scalable_csv_path = Examples.CSV_N3M3.value
     elif (N == 4 and M == 3):
         scalable_csv_path = Examples.CSV_N4M3.value
-    elif (N == 5 and M == 3):
-        scalable_csv_path = Examples.CSV_N5M3.value
     elif (N == 1 and M == 4):
         scalable_csv_path = Examples.CSV_N1M4.value
     elif (N == 2 and M == 4):
         scalable_csv_path = Examples.CSV_N2M4.value
     elif (N == 3 and M == 4):
         scalable_csv_path = Examples.CSV_N3M4.value
-    elif (N == 4 and M == 4):
-        scalable_csv_path = Examples.CSV_N4M4.value
-    elif (N == 5 and M == 4):
-        scalable_csv_path = Examples.CSV_N5M4.value
     elif (N == 1 and M == 5):
         scalable_csv_path = Examples.CSV_N1M5.value
     elif (N == 2 and M == 5):
         scalable_csv_path = Examples.CSV_N2M5.value
     elif (N == 1 and M == 6):
         scalable_csv_path = Examples.CSV_N1M6.value
-    elif (N == 2 and M == 6):
-        scalable_csv_path = Examples.CSV_N2M6.value
 
     return pd.read_csv(scalable_csv_path)
