aa476159ae3b034d5b0e7bf690770dacabd2baf0
Daniel Lawand <daniel.lawand@gmail.com>
2025-07-07 09:30:34 -0300

    change prints

--- causal_reasoning/interventional_do_calculus_algorithm/scalable_problem_init.py
+++ causal_reasoning/interventional_do_calculus_algorithm/scalable_problem_init.py
@@ -158,13 +158,13 @@ def testBetaVars():
     #     #print(f"For betaVar {ind}")
     #     #print("Bit plus:")
     #     for bitPlus in betaVar[0]:
-    # print(f"{bitPlus} ", end="")
-    # print("\n")
+    #       print(f"{bitPlus} ", end="")
+    #       print("\n")
 
     # #print("Bit minus:")
     # for bitMinus in betaVar[1]:
-    # print(f"{bitMinus} ", end="")
-    # print("\n")
+    #   print(f"{bitMinus} ", end="")
+    #   print("\n")
 
     # print("Check coefficients:")
     checkCoefs: list[float] = []
@@ -182,14 +182,14 @@ def testBetaVars():
         checkCoefs.append(prob)
 
     # for ind, coef in enumerate(betaVarsCoeffObjSubproblem):
-    # print(f"#{ind} coef = {coef} | check = {checkCoefs[ind]}")
+    #   print(f"#{ind} coef = {coef} | check = {checkCoefs[ind]}")
 
 
 def testParametricColumns():
     parametricColumns = InitScalable.defineParametricColumn(M=2, N=2)
     # for index, row in enumerate(parametricColumns):
-    # print(f"Analyse row {index}")
-    # print(row)
+    #   print(f"Analyse row {index}")
+    #   print(row)
 
 
 if __name__ == "__main__":
