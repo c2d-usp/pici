b885dc3fe9af640886f6f4faafdc3b71ec0ea6b9
Daniel Lawand <daniel.lawand@gmail.com>
2025-08-11 18:03:00 -0300

    Separate bcause test

--- tests/test_inference_queries.py
+++ tests/test_inference_queries.py
@@ -4,7 +4,6 @@ import unittest
 
 import networkx as nx
 import pandas as pd
-import numpy as np
 import logging
 
 THIS_DIR = os.path.dirname(__file__)
@@ -43,7 +42,7 @@ class TestInferenceAlgorithm(unittest.TestCase):
             msg="Balke-Pearl: intervened graph should d-separate Z and Y given X",
         )
 
-        lower, upper = model.partially_identifiable_intervention_query()
+        lower, upper = model.intervention_query()
         expected_lower, expected_upper = (0.45000000000000007, 0.5199999999999999)
         self.assertAlmostEqual(
             float(lower),
@@ -73,7 +72,7 @@ class TestInferenceAlgorithm(unittest.TestCase):
         model.set_interventions([("X", 2)])
         model.set_target(("Y", 1))
 
-        lower, upper = model.partially_identifiable_intervention_query()
+        lower, upper = model.intervention_query()
         expected_lower, expected_upper = (0.17221135029354206, 0.8160779537149818)
         self.assertAlmostEqual(
             float(lower),
@@ -114,7 +113,7 @@ class TestInferenceAlgorithm(unittest.TestCase):
             model.are_d_separated_in_complete_graph(["E"], ["X"], ["D"]),
             msg="Copilot: complete graph should not d-separate E and X given D",
         )
-        lower, upper = model.partially_identifiable_intervention_query()
+        lower, upper = model.intervention_query()
         expected_lower, expected_upper = (0.3570355041940286, 0.8560355041940286)
         self.assertAlmostEqual(
             float(lower),
@@ -160,7 +159,7 @@ class TestInferenceAlgorithm(unittest.TestCase):
         expected_lower, expected_upper = (0.07680001854838515, 0.09309998464330864)
         expected_lower = round(expected_lower, 3)
         expected_upper = round(expected_upper, 3)
-        lower, upper = model.partially_identifiable_intervention_query()
+        lower, upper = model.intervention_query()
         lower = round(float(lower), 3)
         upper = round(float(upper), 3)
         self.assertEqual(lower, expected_lower)
@@ -217,103 +216,6 @@ class TestInferenceAlgorithm(unittest.TestCase):
             model.are_d_separated_in_complete_graph(["MS-A_Latency"], ["DB_Change"], ["DB_Latency"]),
             msg="Incident scenario: MS-A_Latency and DB_Change should be d-separated given DB_Latency",
         )
-
-class TestBenchmarkBcauseInference(unittest.TestCase):
-    def test_bcause_1(self):
-        edges = "X -> Y, U -> X, U -> Y"
-        unobs = ["U"]
-        df = pd.read_csv(os.path.join(PROJECT_ROOT, DataExamplesPaths.CSV_BCAUSE_EXAMPLE_1.value))
-        bcause = {0: [0.12651440458851518, 0.41206478674401825], 
-                  1: [0.4153088975671237, 0.7948002669191389]}
-
-        for intervention_value in [0, 1]:
-            with self.subTest(intervention_value=intervention_value):
-
-                model = CausalModel(
-                    data=df,
-                    edges=edges,
-                    unobservables_labels=unobs
-                )
-
-                lower, upper = model.partially_identifiable_intervention_query(
-                    interventions=[("X", intervention_value)],
-                    target=("Y", 1)
-                )
-                tv = model.identifiable_intervention_query(
-                    interventions=[("X", intervention_value)],
-                    target=("Y", 1)
-                )
-                self.assertGreaterEqual(
-                    float(upper),
-                    tv,
-                    msg=f"upper bound too low for do(X={intervention_value}) on Y=1"
-                )
-                self.assertLessEqual(
-                    float(lower),
-                    tv,
-                    msg=f"lower bound too high for do(X={intervention_value}) on Y=1"
-                )
-                self.assertAlmostEqual(
-                    float(lower),
-                    bcause[intervention_value][0],
-                    delta=0.3,
-                    msg=f"lower bound does not match for do(X={intervention_value}) on Y=1"
-                )
-                self.assertAlmostEqual(
-                    float(upper),
-                    bcause[intervention_value][1],
-                    delta=0.3,
-                    msg=f"upper bound does not match for do(X={intervention_value}) on Y=1"
-                )
-
-    def test_bcause_2(self):
-
-        edges = "X -> Y, U -> X, U -> Y, X -> W, W -> Y, U2 -> W"
-        unobs = ["U", "U2"]
-        df = pd.read_csv(os.path.join(PROJECT_ROOT, DataExamplesPaths.CSV_BCAUSE_EXAMPLE_2.value))
-        df["U2"] = np.random.binomial(1, 0.5, size=len(df))
-        bcause = {0: [0.3991619515659455, 0.39916195179153174],
-                  1: [0.7014971028665108, 0.7014971031112013]}
-
-        for intervention_value in [0, 1]:
-            with self.subTest(intervention_value=intervention_value):
-                
-                model_2 = CausalModel(
-                    data=df,
-                    edges=edges,
-                    unobservables_labels=unobs
-                )
-
-                lower, upper = model_2.partially_identifiable_intervention_query(
-                    interventions=[("X", intervention_value)],
-                    target=("Y", 1)
-                )
-                tv = model_2.identifiable_intervention_query(
-                    interventions=[("X", intervention_value)],
-                    target=("Y", 1)
-                )
-                self.assertGreaterEqual(
-                    float(upper),
-                    tv,
-                    msg=f"upper bound too low for do(X={intervention_value}) on Y=1"
-                )
-                self.assertLessEqual(
-                    float(lower),
-                    tv,
-                    msg=f"lower bound too high for do(X={intervention_value}) on Y=1"
-                )
-                self.assertAlmostEqual(
-                    float(lower),
-                    bcause[intervention_value][0],
-                    delta=0.3,
-                    msg=f"lower bound does not match for do(X={intervention_value}) on Y=1"
-                )
-                self.assertAlmostEqual(
-                    float(upper),
-                    bcause[intervention_value][1],
-                    delta=0.3,
-                    msg=f"upper bound does not match for do(X={intervention_value}) on Y=1"
-                )
 
 
 if __name__ == "__main__":
