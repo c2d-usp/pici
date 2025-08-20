5cc96d44dcf9c53a369a35f4b0225be541385a86
Daniel Lawand <daniel.lawand@gmail.com>
2025-08-20 14:14:57 -0300

    Minor Updates

--- pici/intervention_inference_algorithm/column_generation/generic/master_problem.py
+++ pici/intervention_inference_algorithm/column_generation/generic/master_problem.py
@@ -52,13 +52,12 @@ class MasterProblem:
             name="EmpiricalRestrictions",
         )
         self.model.modelSense = GRB.MINIMIZE
-        # Turning off output because of the iterative procedure
         self.model.params.outputFlag = 0
-        # self.model.setParam('FeasibilityTol', 1e-9)
+        self.model.setParam('FeasibilityTol', 1e-9)
         self.model.update()
 
     def update(
-        self, new_column: list[float], index: int, obj_coeff: list[float], minimun: bool
+        self, new_column: list[float], index: int, obj_coeff: list[float], minimizes_objective_function: bool
     ):
         """
         Adds a new column (variable) to the constraints in the master problem and updates the model.
@@ -67,25 +66,23 @@ class MasterProblem:
             new_column (list[float]): The coefficients of the new variable for each constraint.
             index (int): The index at which to add the new variable.
             obj_coeff (list[float]): The objective coefficient(s) in the objective function for the new variable.
-            minimun (bool): If True, the objective is minimized; if False, the coefficient is negated for maximization.
+            minimizes_objective_function (bool): If True, the objective is minimized; if False, the coefficient is negated for maximization.
 
         This method constructs a new Gurobi variable with the specified column and objective coefficient,
         adds it to the model, and updates the model structure.
         """
+        if not minimizes_objective_function:
+            obj_coeff = -1*obj_coeff
+
         new_col = gp.Column(
             coeffs=new_column, constrs=self.constrs.values()
-        )  # Includes the new variable in the constraints
+        )
+
         logger.debug(f"Obj coeff: {obj_coeff}")
-        if minimun:
-            self.vars[index] = self.model.addVar(
-                obj=obj_coeff,
-                column=new_col,  # Adds the new variable
-                name=f"Variable[{index}]",
-            )
-        else:
-            self.vars[index] = self.model.addVar(
-                obj=-obj_coeff,
-                column=new_col,  # Adds the new variable
-                name=f"Variable[{index}]",
-            )
+
+        self.vars[index] = self.model.addVar(
+            obj=obj_coeff,
+            column=new_col,
+            name=f"Variable[{index}]",
+        )
         self.model.update()
