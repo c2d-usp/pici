6f66c2e286bdecccfbc23f82bc559abac2b20ac9
Daniel Lawand <daniel.lawand@gmail.com>
2025-10-22 15:29:55 -0300

    Add debugger

--- pici/intervention_inference_algorithm/column_generation/subproblem.py
+++ pici/intervention_inference_algorithm/column_generation/subproblem.py
@@ -26,10 +26,6 @@ if PROJECT_ROOT not in sys.path:
 
 sys.path.append(os.path.abspath(os.path.join(THIS_DIR, PROJECT_ROOT)))
 
-BIG_M = 1e4
-DBG = False
-MAX_ITERACTIONS_ALLOWED = 2000
-
 
 class SubProblem:
     def __init__(self, intervention: Node, target: Node, df: DataFrame = None, minimizes_objective_function=False):
@@ -54,16 +50,16 @@ class SubProblem:
         self.model.setParam(GRB.Param.OutputFlag, 0)
         self.model.setParam(GRB.Param.BestBdStop, 1)
 
-        self.coluna_parametrizada = {}
+        self.parameterized_column = {}
 
         self.reversed_ordered_W = reversed_ordered_W
         self.reversed_ordered_considered_c_comp = reversed_ordered_considered_c_comp
         self.reversed_ordered_W_realizations = reversed_ordered_W_realizations
 
-        print("________________________________________________________________________________")
-        print("FO Symbolic: ------")
+        logger.debug("________________________________________________________________________________")
+        logger.debug("FO Symbolic: ------")
         for element in symbolic_objective_function_probabilites:
-            print(element)
+            logger.debug(element)
 
         realizacao_conjunto_estranho = get_node_list_realizations(conjunto_estranho)
         self._create_cluster_bits(reversed_ordered_considered_c_comp)
@@ -72,42 +68,35 @@ class SubProblem:
         self.objective_function_vars_not_in_W = self.get_objective_function_vars_not_in_W(symbolic_objective_function_probabilites, reversed_ordered_W)
 
         self.Pw, self.Pq = self.separate_objective_function_probabilities(symbolic_objective_function_probabilites, reversed_ordered_W)
-        print("________________________________________________________________________________")
-        print("PQ: ------")
-        print(f"{self.Pq}")
-        print("________________________________________________________________________________")
-        print("PW: ------")
-        print(f"{self.Pw}")
         self.realization_objective_function_vars_not_in_W = get_node_list_realizations(self.objective_function_vars_not_in_W)
 
         self.gamma_u_map_bit_product_to_linearized_variable: dict[BitProduct, Var] = self.gamma_linearize(reversed_ordered_considered_c_comp, realizacao_conjunto_estranho)
         self.generate_linearized_bit_products_constraints(self.gamma_u_map_bit_product_to_linearized_variable, name="gamma")
 
-        self.w_u_map_bit_product_to_linearized_variable: dict[BitProduct, Var] = {}
+        self.a_u_map_bit_product_to_linearized_variable: dict[BitProduct, Var] = {}
 
-        self.get_A_u_parametriza_colunas_matriz_restricoes(reversed_ordered_W_realizations, reversed_ordered_considered_c_comp)
-        self.generate_linearized_bit_products_constraints(self.w_u_map_bit_product_to_linearized_variable, name="au")
+        self.get_A_u_column_and_parameterized_column(reversed_ordered_W_realizations, reversed_ordered_considered_c_comp)
+        self.generate_linearized_bit_products_constraints(self.a_u_map_bit_product_to_linearized_variable, name="au")
         self.model.update()
 
-        # Ver os bits e os sinais de Gamma U e de Au
-        # Mais o coeficiente de Gamma
-        print("________________________________________________________________________________")
-        print("GammaU: ------")
+
+        logger.debug("________________________________________________________________________________")
+        logger.debug("GammaU: ------")
         i = 0
         for bit_product, _ in self.gamma_u_map_bit_product_to_linearized_variable.items():
             str_prod_bit = f"{bit_product.coef} * "
             for b in bit_product.bit_list:
                 str_prod_bit += f"({b.sign} * {b.gurobi_var.VarName}) * "
-            print(f"Element {i}th: {str_prod_bit[:len(str_prod_bit)-3]},")
+            logger.debug(f"Element {i}th: {str_prod_bit[:len(str_prod_bit)-3]},")
             i += 1
-        print("________________________________________________________________________________")
-        print("Au: ------")
-        for bit_product, _ in self.w_u_map_bit_product_to_linearized_variable.items():
+        logger.debug("________________________________________________________________________________")
+        logger.debug("Au: ------")
+        for bit_product, _ in self.a_u_map_bit_product_to_linearized_variable.items():
             str_prod_bit = f""
             for b in bit_product.bit_list:
                 str_prod_bit += f"({b.sign} * {b.gurobi_var.VarName}) * "
             
-            print(f"{str_prod_bit[:len(str_prod_bit)-3]}, ")
+            logger.debug(f"{str_prod_bit[:len(str_prod_bit)-3]}, ")
 
     def _create_cluster_bits(self, considered_c_comp: list[Node]):
         """
@@ -120,18 +109,18 @@ class SubProblem:
 
         """
         j = 0
-        print(f"Cluster Bits of :{considered_c_comp}")
+        logger.debug(f"Cluster Bits of :{considered_c_comp}")
         for node in considered_c_comp:
             parents_without_latent = [parent for parent in node.parents if not parent.is_latent]
             reversed_ordered_node_parents_realizations: list[list] = get_node_list_realizations(parents_without_latent)
             header = reversed_ordered_node_parents_realizations[0]
-            print(f"{header}")
+            logger.debug(f"Parents: {header}")
             reversed_ordered_node_parents_realizations = reversed_ordered_node_parents_realizations[1:]
             node_number_of_bits = math.ceil(math.log2(node.cardinality))
             self.cluster_bits[node.label] = {}
             for i, realization in enumerate(reversed_ordered_node_parents_realizations):
                 realization_key: str = self.get_realization_key(header, realization)
-                print(f"    {j}th - Node {node.label} Realization key: {realization_key}--{realization}")
+                logger.debug(f"    {j}th - Node {node.label} Realization key: {realization_key}--{realization} ==> {node_number_of_bits} bits")
                 j+=1
                 self.cluster_bits[node.label][realization_key] = self.model.addVars(
                     node_number_of_bits, obj=0, vtype=GRB.BINARY, name=f"bit_realization_{i}th_of_node_{node.label}_{realization_key}"
@@ -155,10 +144,14 @@ class SubProblem:
                 realization_key: str = self.get_realization_key(header, realization)
                 logger.debug(f"    {j}th - Constraint of Node {node.label} Realization key: {realization_key}--{realization}")
                 j+=1
+                debug_expr_str = ""
                 expr = 0
-                for i in range(node_number_of_bits-1, -1, -1):
-                    variable_index = node_number_of_bits-i-1
+                for i in range(node_number_of_bits):
+                    variable_index = i
                     expr += 2**variable_index * self.cluster_bits[node.label][realization_key][variable_index]
+                    debug_expr_str += f"2^{variable_index} * b{i}"
+                debug_expr_str += f"<= {node.cardinality-1}"
+                logger.debug(debug_expr_str)
                 self.model.addConstr(expr <= node.cardinality-1, name=f"discrete_constraint_of_node_{node.label}_{realization_key}")
 
 
@@ -201,7 +194,7 @@ class SubProblem:
         Change the objective functions coefficients.
         """
         self.model.setAttr(
-            "obj", self.coluna_parametrizada, [-duals[dualKey] for dualKey in duals]
+            "obj", self.parameterized_column, [-duals[dualKey] for dualKey in duals]
         )
         self.model.update()
     
@@ -232,10 +225,10 @@ class SubProblem:
 
     def get_coef_from_objective_function(self, w_header: list, w_realization: list):
         coefw = 1
-        print("_____________________________________")
-        print("W")
-        print(f"{w_header}")
-        print(f"{w_realization}")
+        logger.debug("Coeficients")
+        logger.debug("W")
+        logger.debug(f"{w_header}")
+        logger.debug(f"{w_realization}")
         for w_conditional_probability in self.Pw:
             w_target, w_conditioned = w_conditional_probability
 
@@ -245,7 +238,7 @@ class SubProblem:
                 node.value = w_realization[w_header.index(node.label)]
                 str_w += f"{node.label}={node.value}, "
             curr = find_conditional_probability(dataFrame=self.df, target_realization=[w_target], condition_realization=w_conditioned)
-            print(f"P({w_target.label}={w_target.value}|{str_w[:len(str_w)-2]}) == {curr}")
+            logger.debug(f"P({w_target.label}={w_target.value}|{str_w[:len(str_w)-2]}) == {curr}")
 
             coefw *= curr
             
@@ -255,16 +248,16 @@ class SubProblem:
         coefq = 0
         q_header = self.realization_objective_function_vars_not_in_W[0]
         q_realizations = self.realization_objective_function_vars_not_in_W[1:]
-        print("_____________________________________")
-        print("Q")
+        logger.debug("_____________________________________")
+        logger.debug("Q")
         for q_realization in q_realizations:
             if self.intervention.label in q_header and q_realization[q_header.index(self.intervention.label)] != self.intervention.intervened_value:
                 continue
             if self.target.label in q_header and q_realization[q_header.index(self.target.label)] != self.target.intervened_value:
                 continue
-            print("--------------------")
-            print(f"{q_header}")
-            print(f"{q_realization}")
+            logger.debug("--------------------")
+            logger.debug(f"{q_header}")
+            logger.debug(f"{q_realization}")
             coef_parcial = 1
             for q_conditional_probability in self.Pq:
                 q_target, q_conditioned = q_conditional_probability
@@ -283,29 +276,28 @@ class SubProblem:
                     str_q += f"{node.label}={node.value}, "
                 curr = find_conditional_probability(dataFrame=self.df, target_realization=[q_target],condition_realization=q_conditioned)
                 coef_parcial *= curr
-                print(f"P({q_target.label}={q_target.value}|{str_q[:len(str_q)-2]}) == {curr}")
-            print(f"Coef_Parcial: {coef_parcial}")
+                logger.debug(f"P({q_target.label}={q_target.value}|{str_q[:len(str_q)-2]}) == {curr}")
+            logger.debug(f"Coef_Parcial: {coef_parcial}")
             coefq += coef_parcial
-            print(f"coefW: {coefw}")
-            print(f"coefq: {coefq}")
-            print(f"coef: {coefw*coefq}")
-            print("----")
+            logger.debug(f"coefW: {coefw}")
+            logger.debug(f"coefq: {coefq}")
+            logger.debug(f"coef: {coefw*coefq}")
+            logger.debug("----")
         return coefq * coefw
 
     def _get_cluster_node_bit_variable_given_parents_realization(self, node: Node, w_realization: list[int], w_header: list[str]) -> list[Var]:
-        # Garantir que a ordem da realization_key está em ordem topologica reversa, a fim de acessar o dicionário
         parents_labels = [parent.label for parent in node.parents if not parent.is_latent]
         parents_realization = [w_realization[w_header.index(parent_label)] for parent_label in parents_labels]
         realization_key: str = self.get_realization_key(parents_labels, parents_realization)
         return self.cluster_bits[node.label][realization_key]
 
-    def get_A_u_parametriza_colunas_matriz_restricoes(self, total_w_realization, considered_c_component_in_topological_order):
+    def get_A_u_column_and_parameterized_column(self, total_w_realization, considered_c_component_in_topological_order):
         header = total_w_realization[0]
         total_w_realization = total_w_realization[1:]
         for i, realization in enumerate(total_w_realization):
             bit_product: BitProduct = self.generate_bit_product(node_list=considered_c_component_in_topological_order, header=header, realization=realization)
-            self.w_u_map_bit_product_to_linearized_variable[bit_product] = self.model.addVar(vtype=GRB.BINARY)
-            self.coluna_parametrizada[i] = self.w_u_map_bit_product_to_linearized_variable[bit_product]
+            self.a_u_map_bit_product_to_linearized_variable[bit_product] = self.model.addVar(vtype=GRB.BINARY)
+            self.parameterized_column[i] = self.a_u_map_bit_product_to_linearized_variable[bit_product]
 
     def generate_bit_product(self, node_list: list[Node], header: list[str], realization: list[int], consider_intervention=False):
             bit_product = BitProduct()
@@ -316,18 +308,17 @@ class SubProblem:
                 node_idx = header.index(node.label)
                 node_value = realization[node_idx]
                 binary_node_value = bin(node_value)[2:]
-                k = math.ceil(math.log2(node.cardinality))
+                node_number_of_bits = math.ceil(math.log2(node.cardinality))
 
-                if len(binary_node_value) > k:
+                if len(binary_node_value) > node_number_of_bits:
                     raise Exception(f"Binary number of node {node.label} is greater than its cardinality")
                 else:
-                    binary_node_value = (k-len(binary_node_value))*"0" + binary_node_value
+                    binary_node_value = (node_number_of_bits-len(binary_node_value))*"0" + binary_node_value
 
-                for i in range(k-1, -1, -1):
+                for variable_index in range(node_number_of_bits):
                     sign = 1
-                    if int(binary_node_value[i]) == 0:
+                    if int(binary_node_value[variable_index]) == 0:
                         sign = -1
-                    variable_index = k-i-1
                     new_bit = Bit(cluster_bit_gurobi_var[variable_index], sign)
                     bit_product.add_bit(new_bit)
             return bit_product
@@ -359,5 +350,4 @@ def get_node_list_realizations(node_list: list[Node]) -> list[list]:
     cartesian = product(*ranges)
     matrix = [[node.label for node in node_list]]
     matrix += [list(combo) for combo in cartesian]
-    # idx_A = matrix[0].index(node.label)
     return matrix
